2d6e56c5205ef64038f3f9a60448f80d80e65cee
Felipe Romero <tmfromero@gmail.com>
2022-08-17 19:39:13 -0400

    Fin de clase 3.2, se revizan más conceptos de los métodos y se crea una petición GET usando la configuración de Axios.

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,12 @@
-axios.get("http://localhost:3000/posts")
+// axios.get("http://localhost:3000/posts")
+// .then(response => console.log(response))
+// .catch(err => console.log(err))
+
+
+axios({
+    url: "/posts",
+    baseURL: "http://localhost:3000",
+    method: "get"
+})
 .then(response => console.log(response))
 .catch(err => console.log(err))
