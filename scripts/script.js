213efcebfbe2907bb05a26f3359579a6f5b51a26
Felipe Romero <tmfromero@gmail.com>
2022-08-17 21:17:02 -0400

    Fin clase 3.3, se agrega método post con axios.post y axios con async await

--- scripts/script.js
+++ scripts/script.js
@@ -10,3 +10,43 @@ axios({
 })
 .then(response => console.log(response))
 .catch(err => console.log(err))
+
+
+const myForm = document.getElementById("myForm")
+const title = document.getElementById("title")
+const author = document.getElementById("author")
+const btnSave = document.getElementById("btnSave")
+
+myForm.addEventListener("submit", e => {
+    e.preventDefault()
+})
+
+const makePost = () => {
+    return {
+        title: title.value,
+        author: author.value
+    }
+}
+
+// Método post con axios.post, then y catch
+// btnSave.addEventListener("click", () => {
+//     const data = makePost()
+//     axios.post("http://localhost/posts", data)
+//     .then(response => console.log(response))
+//     .catch(error => console.log(error))
+// })
+
+//Método post con axio, async await y try catch
+btnSave.addEventListener("click", async () => {
+    const data = makePost()
+    try {
+    const response = await axios({
+        metodh: "post",
+        url: "http://localhost/posts", 
+        data: data
+    })
+    console.log(response)
+    } catch (error){
+        console.log(error)
+    }
+})
